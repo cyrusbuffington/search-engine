4086d477a291f24bf9fcdae632ecdf5b68d94244
cyrusbuffington <cyrusbuffington@gmail.com>
2024-05-06 18:21:29 -0700

    simple json processing

--- index.py
+++ index.py
@@ -1,1 +1,18 @@
-print("hello word")
+import os
+import json
+
+def process_json_files(folder_path):
+    'Yields data of each JSON file in folder_path'
+    for root, dirs, files in os.walk(folder_path):
+        for filename in files:
+            if filename.endswith('.json'):
+                with open(os.path.join(root, filename), 'r') as f:
+                    data = json.load(f)
+                    yield data
+
+if __name__ == '__main__':
+    page_count = 0
+    for page in process_json_files('ANALYST/'):
+        page_count += 1
+        print(page['url'])
+    print(page_count)
