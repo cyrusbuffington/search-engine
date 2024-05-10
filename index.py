a7e7fd4f18391bd515ca36b0d4eec2a60af3c648
cyrusbuffington <cyrusbuffington@gmail.com>
2024-05-10 14:01:33 -0700

    Implemented pickle for smaller index files

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 import os
 import json
 import hashlib
-from nltk.stem import PorterStemmer
+import pickle
+from porter2stemmer import Porter2Stemmer
 from nltk.tokenize import RegexpTokenizer
 from bs4 import BeautifulSoup
 from collections import defaultdict
@@ -29,15 +30,16 @@ def tokenize(text_content):
     #Build token and process token when encounter non alphanum char
     token = ""
     for char in text_content:
-        if len(char) != 1:
-            continue
         char = char.lower()
-        if (ord(char)>=97 and ord(char)<=122) or (ord(char)>=48 and ord(char)<=57) or ord(char)==39:
-            token += char
-        else:
-            tokens.append(token)
-            token = ""
-    if token:
+        try:
+            if (ord(char)>=97 and ord(char)<=122) or (ord(char)>=48 and ord(char)<=57) or ord(char)==39:
+                token += char
+            elif len(token)>1:
+                tokens.append(token)
+                token = ""
+        except TypeError:
+            continue 
+    if len(token)>1:
         tokens.append(token)
 
     return tokens
@@ -46,20 +48,23 @@ def remove_fragment(url):
     'Removes the fragment part of a URL'
     return url.split('#')[0]
     
+def default_factory():
+    return defaultdict(int)
 
 def build_partial_indexes(folder_path, threshold):
     'Builds partial indexes and saves them to disk'
-    inverted_index = defaultdict(lambda: defaultdict(int))
+    inverted_index = defaultdict(default_factory)
     file_counter = 0
+    page_counter = 0
 
-    tokenizer = RegexpTokenizer(r'[a-zA-Z0-9]+')  #Matches any sequence of word characters
-    stemmer = PorterStemmer()
+    tokenizer = RegexpTokenizer(r'[a-zA-Z0-9]+')  #Matches any sequence of alphanum characters
+    stemmer = Porter2Stemmer()
 
-    total_pages = 0
     universal_tokens = set()
     url_dict = {} #Stores hashes of URLs to save disk space
 
     for page in process_json_files(folder_path):
+        page_counter += 1
         tokens = tokenizer.tokenize(get_text_content(page))
 
         url = remove_fragment(page['url'])
@@ -73,27 +78,23 @@ def build_partial_indexes(folder_path, threshold):
             universal_tokens.add(stemmed_token)
             inverted_index[stemmed_token][url_hash] += 1
 
-        if len(inverted_index) >= threshold:
-            with open(f'indexes/partial_index_{file_counter}.json', 'w') as f:
-                json.dump(inverted_index, f) #Dump the inverted index
-            with open(f'ids/url_dict_{file_counter}.json', 'w') as f:
-                json.dump(url_dict, f)  #Dump the URL dictionary
+        if page_counter % threshold == 0:
+            with open(f'indexes/partial_index_{file_counter}.pkl', 'wb') as f:
+                pickle.dump(inverted_index, f) #Dump the inverted index
             inverted_index.clear() #Clear the inverted index
             url_dict.clear()  #Clear the URL dictionary
             file_counter += 1
 
-        total_pages += 1
-
-    if inverted_index:
-        with open(f'indexes/partial_index_{file_counter}.json', 'w') as f:
-            json.dump(inverted_index, f)
-        with open(f'ids/url_dict_{file_counter}.json', 'w') as f:
-            json.dump(url_dict, f) 
+    if inverted_index or url_dict:
+        with open(f'indexes/partial_index_{file_counter}.pkl', 'wb') as f:
+            pickle.dump(inverted_index, f) #Dump the inverted index
+        with open(f'url_hashes.pkl', 'wb') as f:
+            pickle.dump(url_dict, f)  #Dump the URL dictionary
 
     #ANALYTICS
-    print(f'Total pages: {total_pages}')
+    print(f'Total pages: {page_counter}')
     print(f'Total words: {len(universal_tokens)}')
 
 
 if __name__ == '__main__':
-    build_partial_indexes('developer', 200000)
+    build_partial_indexes('developer/DEV', 15000)
